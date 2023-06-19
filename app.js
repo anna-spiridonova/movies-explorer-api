b95b08500179b067d7ae054d4ae2dec61bb0311c
Anna Spiridonova <spiridonova.aniutka@yandex.ru>
2023-06-19 19:29:39 +0300

    feat: реализовать логирование

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 // const { errors } = require('celebrate');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
-// const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -16,8 +16,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
 app.use(cookieParser());
 app.use(express.json());
-
+app.use(requestLogger);
 app.use(router);
+app.use(errorLogger);
 app.use(errorHandler);
 
 app.listen(PORT);
